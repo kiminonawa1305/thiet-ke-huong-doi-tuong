313576f4993ac1526e91e31fb18744cae0ba6967
Lam Nguyen <kiminonawa1305@gmail.com>
2023-03-09 19:34:37 +0700

    cải thiện code phần tính và vẽ kim đồng hồ file Analog.java

--- BaiTapTrenLop/src/Time/Analog.java
+++ BaiTapTrenLop/src/Time/Analog.java
@@ -1,9 +1,11 @@
 package Time;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.util.Date;
 import java.util.Observable;
@@ -21,99 +23,6 @@ public class Analog extends JPanel implements Observer {
 		observable.addObserver(this);
 	}
 
-	public int[] timToaDoGiay() {
-		int re[] = new int[2];
-
-		// Tọa độ tâm
-		int cx = 200;
-		int cy = 200;
-
-		// Tọa độ điểm trên đường tròn
-		int x1 = 200;
-		int y1 = 20;
-//		System.out.println(cx + ", " + cy + "; " + x1 + ", " + y1);
-
-		// Chuyển đổi đơn vị độ sang radian
-		double angleInRadians = Math.toRadians(this.second*6 - 90);
-
-		// Tính khoảng cách từ tâm đến điểm trên đường tròn
-		double r = Math.sqrt(Math.pow(cx - x1, 2) + Math.pow(cy - y1, 2));
-
-		// Tọa độ đỉnh thứ hai
-		int x2 = (int) Math.round(cx + r * Math.cos(angleInRadians));
-		int y2 = (int) Math.round(cy + r * Math.sin(angleInRadians));
-
-		// In kết quả
-//		System.out.println("Tọa độ của đỉnh thứ hai tại góc: " + angle + " là: (" + x2 + ", " + y2 + ")");
-
-		re[0] = x2;
-		re[1] = y2;
-
-		return re;
-	}
-	
-	public int[] timToaDoPhut() {
-		int re[] = new int[2];
-
-		// Tọa độ tâm
-		int cx = 200;
-		int cy = 200;
-
-		// Tọa độ điểm trên đường tròn
-		int x1 = 200;
-		int y1 = 40;
-//		System.out.println(cx + ", " + cy + "; " + x1 + ", " + y1);
-
-		// Chuyển đổi đơn vị độ sang radian
-		double angleInRadians = Math.toRadians(this.minute*6 - 90);
-
-		// Tính khoảng cách từ tâm đến điểm trên đường tròn
-		double r = Math.sqrt(Math.pow(cx - x1, 2) + Math.pow(cy - y1, 2));
-
-		// Tọa độ đỉnh thứ hai
-		int x2 = (int) Math.round(cx + r * Math.cos(angleInRadians));
-		int y2 = (int) Math.round(cy + r * Math.sin(angleInRadians));
-
-		// In kết quả
-//		System.out.println("Tọa độ của đỉnh thứ hai tại góc: " + angle + " là: (" + x2 + ", " + y2 + ")");
-
-		re[0] = x2;
-		re[1] = y2;
-
-		return re;
-	}
-	
-	public int[] timToaDoGio() {
-		int re[] = new int[2];
-
-		// Tọa độ tâm
-		int cx = 200;
-		int cy = 200;
-
-		// Tọa độ điểm trên đường tròn
-		int x1 = 200;
-		int y1 = 60;
-//		System.out.println(cx + ", " + cy + "; " + x1 + ", " + y1);
-
-		// Chuyển đổi đơn vị độ sang radian
-		double angleInRadians = Math.toRadians(this.hour*6 - 90);
-
-		// Tính khoảng cách từ tâm đến điểm trên đường tròn
-		double r = Math.sqrt(Math.pow(cx - x1, 2) + Math.pow(cy - y1, 2));
-
-		// Tọa độ đỉnh thứ hai
-		int x2 = (int) Math.round(cx + r * Math.cos(angleInRadians));
-		int y2 = (int) Math.round(cy + r * Math.sin(angleInRadians));
-
-		// In kết quả
-//		System.out.println("Tọa độ của đỉnh thứ hai tại góc: " + angle + " là: (" + x2 + ", " + y2 + ")");
-
-		re[0] = x2;
-		re[1] = y2;
-
-		return re;
-	}
-	
 	@Override
 	public void update(Observable o, Object arg) {
 		if (o instanceof Time) {
@@ -123,21 +32,39 @@ public class Analog extends JPanel implements Observer {
 			this.hour = time.getHour();
 		}
 	}
-	
-	
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.white);
-		g.fillOval(0, 0, 400, 400);
-		int toaDoGiay[] = this.timToaDoGiay();
-		int toaDoPhut[] = this.timToaDoPhut();
-		int toaDoGio[] = this.timToaDoGio();
-		g.setColor(Color.black);
-		g.drawLine(200, 200, toaDoGiay[0], toaDoGiay[1]);
-		g.drawLine(200, 200, toaDoPhut[0], toaDoPhut[1]);
-		g.drawLine(200, 200, toaDoGio[0], toaDoGio[1]);
+		Graphics2D g2d = (Graphics2D) g;
+
+		Point toaDoKiemGio = new Point();
+		Point toaDoKiemPhut = new Point();
+		Point toaDoKiemGiay = new Point();
+		// Chuyển đổi đơn vị độ sang radian
+		double gocXoayKiemGio = Math.toRadians(this.hour * 15 - 90);
+		double gocXoayKiemPhut = Math.toRadians(this.minute * 6 - 90);
+		double gocXoayKiemGiay = Math.toRadians(this.second * 6 - 90);
+
+		// Tính khoảng cách từ tâm đến điểm trên đường tròn
+		toaDoKiemGio.x = (int) Math.round(200 + 140 * Math.cos(gocXoayKiemGio));
+		toaDoKiemGio.y = (int) Math.round(200 + 140 * Math.sin(gocXoayKiemGio));
+		toaDoKiemPhut.x = (int) Math.round(200 + 155 * Math.cos(gocXoayKiemPhut));
+		toaDoKiemPhut.y = (int) Math.round(200 + 155 * Math.sin(gocXoayKiemPhut));
+		toaDoKiemGiay.x = (int) Math.round(200 + 170 * Math.cos(gocXoayKiemGiay));
+		toaDoKiemGiay.y = (int) Math.round(200 + 170 * Math.sin(gocXoayKiemGiay));
+
+		g2d.setStroke(new BasicStroke(4));
+
+		// Vẽ mặt đồng hồ.
+		g2d.setColor(Color.BLACK);
+		g2d.drawOval(10, 10, 390, 390);
+
+		g2d.setColor(Color.black);
+		// Vẽ kim giây.
+		g2d.drawLine(200, 200, toaDoKiemGiay.x, toaDoKiemGiay.y);
+		g2d.drawLine(200, 200, toaDoKiemPhut.x, toaDoKiemPhut.y);
+		g2d.drawLine(200, 200, toaDoKiemGio.x, toaDoKiemGio.y);
 		this.repaint();
 	}
 
